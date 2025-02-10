2e05bc81a6dece40cc1ddb2edaae43a007978501
Vojta Vorel <vorelvo2@uhk.cz>
2025-02-10 17:10:35 +0100

    Další ahoj

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -2,6 +2,7 @@ package pro1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ahoj");
+        System.out.println("Ahoj 1");
+        System.out.println("Ahoj 2");
     }
 }
