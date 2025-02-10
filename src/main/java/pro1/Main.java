05628ffa9c77879d415a04ff08b7238187fe4fbf
Vojta Vorel <vorelvo2@uhk.cz>
2025-02-10 17:51:00 +0100

    Data z maturit

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -2,7 +2,7 @@ package pro1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ahoj 1");
-        System.out.println("Ahoj 2");
+        System.out.println(0.3);
+        System.out.println(0.1+0.2);
     }
 }
