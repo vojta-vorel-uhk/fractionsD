dc41dfd96a71550b32bbb95eecb9fd330adf6611
Vojta Vorel <vorelvo2@uhk.cz>
2025-03-03 17:52:43 +0100

    Parsování (test prochází)

--- src/main/java/pro1/Fraction.java
+++ src/main/java/pro1/Fraction.java
@@ -1,5 +1,7 @@
 package pro1;
 
+import java.util.regex.Pattern;
+
 public class Fraction
 {
     private long n; // Čitatel
@@ -11,8 +13,35 @@ public class Fraction
         this.d = d / g;
     }
 
-    public static Fraction parse(String s) {
-        return new Fraction(1,1);
+    public Fraction add(Fraction other){
+        return new Fraction(
+                n*other.d + other.n*d,
+                d * other.d
+        );
+    }
+
+    public static Fraction parse(String s)
+    {
+        s = s.replace(" ","");
+        String[] split = s.split(Pattern.quote("+"));
+        Fraction sum = new Fraction(0,1);
+        for(String part : split)
+        {
+            Fraction partFraction;
+            if(part.contains("%")){
+                partFraction = new Fraction(
+                      Long.parseLong(part.replace("%","")),
+                      100);
+            }
+            else{
+                String[] split2 = part.split("/");
+                partFraction = new Fraction(
+                    Long.parseLong(split2[0]),
+                    Long.parseLong(split2[1]));
+            }
+            sum = sum.add(partFraction);
+        }
+        return sum;
     }
 
     public String toString() {
